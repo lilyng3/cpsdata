b1464505c14d3dffeaca38ac58f16d9ef3629b25
lilyng3 <lilyng2024@u.northwestern.edu>
2023-12-05 12:10:29 -0600

    added all races

--- cps-data/app.R
+++ cps-data/app.R
@@ -5,7 +5,6 @@ library(ggthemes)
 
 cps <- read_rds("data/cps_data.rds")
 
-
 # data manipulation -------------------------------------------------------
 # by race
 racial_groups <- c("student_count_black", "student_count_hispanic", "student_count_white", "student_count_asian", "student_count_native_american", "student_count_other_ethnicity","student_count_asian_pacific_islander", "student_count_multi", "student_count_hawaiian_pacific_islander", "student_count_ethnicity_not_available")
@@ -24,11 +23,9 @@ cps_sorted <- cps |>
     primary_math = names(cps[, math_score])[max.col(cps[, math_score], "last")]
   ) |> 
   mutate(primary_race = sub("student_count_", "", primary_race),
-         percent_low_income = round((student_count_low_income / student_count_total) * 100, 1)) |>
-  # omitted due to NA values for ELA in one year, chose to remove whole school for continuity
-  filter(school_name != "U OF C - WOODLAWN HS")
-
-cps_sorted <- cps_sorted |> 
+         percent_low_income = round((student_count_low_income / student_count_total) * 100, 1),
+         title_one = percent_low_income >= 40) |>
+  filter(school_name != "U OF C - WOODLAWN HS") |> 
   mutate(
     primary_race = factor(primary_race),
     primary_ela = factor(primary_ela),
@@ -42,28 +39,13 @@ pandemic_scores_race <- cps_sorted |>
     avg_met_math = mean(percent_met_math, na.rm = TRUE)
   )
 
-cps_lowincome <- cps_sorted |> 
-  filter(percent_low_income >= 40)
-
-pandemic_scores_race_lowincome <- cps_lowincome |> 
-  group_by(year, primary_race) |> 
+pandemic_scores_race_lowincome <- cps_sorted |>
+  filter(title_one == TRUE) |>
+  group_by(year, primary_race) |>
   summarise(
     avg_met_ela = mean(percent_met_ela, na.rm = TRUE),
     avg_met_math = mean(percent_met_math, na.rm = TRUE)
   )
-
-cps_asian <- cps_sorted |> 
-  filter(primary_race == "asian")
-
-cps_black <- cps_sorted |> 
-  filter(primary_race == "black")
-
-cps_hispanic <- cps_sorted |> 
-  filter(primary_race == "hispanic")
-
-cps_white <- cps_sorted |> 
-  filter(primary_race == "white")
-
 
 # shiny app ---------------------------------------------------------------
 
@@ -84,6 +66,7 @@ ui <- fluidPage(
       radioButtons("subject_selector", "Select Subject", choices = c("ELA", "Math"), selected = "ELA"),
       
       checkboxInput("low_income_selector", "Include Low Income Data", value = FALSE),
+      checkboxInput("all_data_selector", "Include All Data", value = FALSE),
       
       tags$p("Data Source: City of Chicago")
     )
@@ -93,64 +76,49 @@ ui <- fluidPage(
 # Define the server logic
 server <- function(input, output) {
   
-  # function to generate the bar plot based on the selected race and subject
+  # function to generate the line plot based on the selected race and subject
   generate_plot <- reactive({
-    race_data <- subset(pandemic_scores_race, primary_race == tolower(input$race_selector))
+    if (input$all_data_selector) {
+      # Use all data
+      data <- pandemic_scores_race
+      low_income_data <- pandemic_scores_race_lowincome
+    } else {
+      # Use selected race data
+      data <- subset(pandemic_scores_race, primary_race == tolower(input$race_selector))
+      low_income_data <- subset(pandemic_scores_race_lowincome, primary_race == tolower(input$race_selector))
+    }
     
     y_variable <- ifelse(input$subject_selector == "ELA", "avg_met_ela", "avg_met_math")
     y_label <- ifelse(input$subject_selector == "ELA", "Average % Met ELA Standards", "Average % Met Math Standards")
     title <- paste("Percentage of Students In Majority", input$race_selector, "Schools Meeting", input$subject_selector, "Standards Over Time")
     
-    custom_colors <- c("#556EE6", "#778BEB", "#F78FB3", "#F8A5C2")  
-    
-    ggplot(race_data, aes(x = as.factor(year), y = !!sym(y_variable), fill = as.factor(year))) +
-      geom_bar(stat = "identity", position = "dodge") +
+    plot <- ggplot(data, aes(x = as.factor(year), y = !!sym(y_variable), color = primary_race, group = primary_race)) +
+      geom_point() +
+      geom_line() +
       labs(title = title,
            x = "Year",
            y = y_label,
-           fill = "Year") +
-      scale_fill_manual(values = custom_colors) +
-      # set y-axis limits to make visual comparison better
-      scale_y_continuous(limits = c(0, 50)) +  
+           color = "Primary Race") +
       theme_minimal() +
+      ylim(0, 50) +
       theme(legend.position = "bottom")
-  })
-  
-  # function to make the low-income subset plot
-  generate_low_income_plot <- reactive({
+    
     if (input$low_income_selector) {
-      race_lowincome_data <- subset(pandemic_scores_race_lowincome, primary_race == tolower(input$race_selector))
-      
-      y_variable <- ifelse(input$subject_selector == "ELA", "avg_met_ela", "avg_met_math")
-      y_label <- ifelse(input$subject_selector == "ELA", "Average % Met ELA Standards", "Average % Met Math Standards")
-      title <- paste("Percentage of Low-Income Students In Majority", input$race_selector, "Schools Meeting", input$subject_selector, "Standards Over Time")
-      
-      custom_colors <- c("#556EE6", "#778BEB", "#F78FB3", "#F8A5C2") 
-      
-      ggplot(race_lowincome_data, aes(x = as.factor(year), y = !!sym(y_variable), fill = as.factor(year))) +
-        geom_bar(stat = "identity", position = "dodge") +
-        labs(title = title,
-             x = "Year",
-             y = y_label,
-             fill = "Year") +
-        scale_fill_manual(values = custom_colors) +
-        scale_y_continuous(limits = c(0, 50)) +  # Set y-axis limits
-        theme_minimal() +
-        theme(legend.position = "bottom")
-    } else {
-      NULL
+      plot <- plot +
+        geom_point(data = low_income_data, aes(x = as.factor(year), y = !!sym(y_variable), color = primary_race), shape = 1) +
+        geom_line(data = low_income_data, aes(x = as.factor(year), y = !!sym(y_variable), group = primary_race), linetype = "dashed")
     }
+    
+    return(plot)
   })
   
-  # average of all scores plot
+  # line plot for average of all scores
   output$score_plot <- renderPlot({
     generate_plot()
   })
   
-  # average of low-income scores plot
-  output$low_income_plot <- renderPlot({
-    generate_low_income_plot()
-  })
+  # ...
+  
 }
 
 # Run the Shiny app
